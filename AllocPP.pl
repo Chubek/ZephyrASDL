5e7576268d122018ff942125fbbad17873711399
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-02-13 21:42:14 +0330

    Replaced: with the correct version for my gist

--- AllocPP.pl
+++ AllocPP.pl
@@ -8,16 +8,8 @@ use Getopt::Std;
 my %opts;
 getopts('i:o:', \%opts);
 
-my $input_file = \*STDIN;
-my $output_file = \*STDOUT;
-
-if ($opts{i}) {
-	open($input_file, '<', $opts{i}); 
-}
-
-if ($opts{o}) {
-	open($output_file, '>', $opts{o});
-}
+my $input_file = $opts{i} || \*STDIN;
+my $output_file = $opts{o} || \*STDOUT;
 
 my @alloc_directives;
 my @bypass_lines;
@@ -27,6 +19,113 @@ my @reallocfns;
 my @dumpfns;
 my $hash_function_name;
 my $hash_function;
+
+
+
+=head1 NAME
+
+AllocPP.pl - Perl script for generating static allocation heaps in C
+
+=head1 SYNOPSIS
+
+    AllocPP.pl [-i input_file] [-o output_file]
+
+=head1 DESCRIPTION
+
+This script scans a C file for specific directives and generates static allocation heaps based on those directives.
+
+The directives are in the style of C preprocessor directives. These directives, and their signature, are as follows:
+
+	#alloc <heap name> , <allocate function name> , <reallocate function name> , <dump function name>
+	#hashfunc <hash function name>
+
+The signature of the generated functions is:
+
+	void* <allocate function>(size_t size);
+	void* <reallocate function>(void* ptr, size_t resize);
+	void  <dump function>(void);
+
+All the directive parameters are mandatory. You can create several heaps, but only one '#hashfunc' directive is necessary.
+
+Basically, the #alloc directive creates an allocation heap, and an allocation, reallocation, and dump function for it. You can use this heap to allocate and reallocate data, and then dump it at the end.
+
+The '#hashfunc' directive names the MurMurHash function that is necessary for the process.
+
+Notice that the functions do safe allocations and reallocations; you do not need to check for errors and null pointers. If a returned pointer for calloc and realloc is NULL, it will error out and exit.
+
+Also, if the size given to realloc is smaller than the already-given size, it will warn the user.
+
+=head1 EXAMPLE
+
+Suppose you have a C file named "example.c" with the following content:
+
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    // Your existing C code here...
+
+    #alloc heap1, allocate1, reallocate1, dump1
+    #alloc heap2, allocate2, reallocate2, dump2
+    #hashfunc my_hash_function
+
+    int main() {
+        void* int ptr1 = allocate1(sizeof(int));
+        void* int ptr2 = allocate2(sizeof(int));
+
+        *ptr1 = 22;
+        *ptr2 = 33;
+
+        ptr1 = reallocate1(ptr1, sizeof(int) * 2);
+        *(ptr1 + sizeof(int)) = 44;
+
+        // do stuff
+
+        dump1();
+        dump2();
+    }
+ 
+=head1 OPTIONS
+
+=over 4
+
+=item B<-i>
+
+Input file. If not provided, it defaults to STDIN.
+
+=item B<-o>
+
+Output file. If not provided, it defaults to STDOUT.
+
+=head1 AUTHOR
+
+This script was written by Chubak Bidpaa (chubakbidpaa@gmail.com).
+
+=head1 COPYRIGHT
+
+GNU GENERAL PUBLIC LICENSE
+Version 3, June 2007
+
+Preamble:
+The GNU General Public License is a free, copyleft license for software and other kinds of works.
+
+Terms and Conditions:
+- You are free to run, modify, and distribute the software.
+- Source code access is required for any distributed modifications.
+- Derivative works must be distributed under the GPL.
+
+Additional Permissions:
+- Propagation of covered works is permitted without modification.
+- Adding exceptions to this license is allowed.
+
+Disclaimer of Warranty:
+The software is provided without any warranty.
+
+Full License Text:
+The complete text of the GNU General Public License version 3.0 can be found at [GNU GPL v3.0](https://www.gnu.org/licenses/gpl-3.0.en.html).
+
+=back
+
+=cut
 
 while (<$input_file>) {
     if (/^#alloc\s+(.+)\s*,\s*([a-zA-Z_][a-zA-Z0-9_]*)\s*,\s*([a-zA-Z_][a-zA-Z0-9_]*),\s*([a-zA-Z_][a-zA-Z0-9_]*)\s*$/) {
@@ -43,10 +142,8 @@ while (<$input_file>) {
     }
 }
 
-$hash_function_name = $hash_function_name || "alloc_hashfn";
-
 sub generate_pointer_hash_function {
-    my $random_integer = int(rand(100)) + 1; 
+    my $random_integer = int(rand(100)) + 1;
     $hash_function = <<"END_HASH_FUNCTION";
 unsigned long long $hash_function_name(void *ptr) {
     const unsigned long long m = 0xc6a4a7935bd1e995;
@@ -101,8 +198,8 @@ void *$alloc_directive->{allocfn_name}(unsigned long long size) {
     node->next = ___HEAP__$alloc_directive->{heap_name};
     void *alloc_error_check = calloc(1, size);
     if (alloc_error_check == NULL) {
-	fprintf(stderr, "Allocation error");
-	fputc(10, stderr);
+        fprintf(stderr, "Allocation error");
+        fputc(10, stderr);
     }
     node->alloc_data = calloc(1, size);
     node->alloc_size = size;
@@ -137,13 +234,14 @@ void *$alloc_directive->{reallocfn_name}(void *ptr, unsigned long long resize) {
                 exit(EXIT_FAILURE);
             } else {
                 node->alloc_data = alloc_error_check;
+		node->alloc_size = resize;
             }
             return alloc_error_check;
         }
     }
 
-    fprintf(stderr, "Error: the pointer does not belong to this heap");
-    fputc(10, stderr);
+    // in case the node does not exist, we alloate it
+    return $alloc_directive->{allocfn_name}(resize);
 }
 END_FUNC
         push @reallocfns, $reallofn_decl;
@@ -197,3 +295,6 @@ print $output_file $hash_function, "\n\n";
 # Print notifications
 print $output_file "\n", "/* The preceding file was preprocessed with AllocPP.pl, a Perl script for generating static allocation heaps in C */", "\n\n";
 
+
+close($input_file) if defined $opts{i};
+close($output_file) if defined $opts{o};
