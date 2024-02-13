99443c9097f21fba99505b44da3a2339d36f42cd
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-02-13 04:58:49 +0330

    In progress: implementation of AST walker, most of the installers have been implemented

--- translate.c
+++ translate.c
@@ -3,7 +3,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-const char memo_tbl[MAX_TBL] = {false};
+char memo_tbl[MAX_TBL] = {false};
+char *curr_id = NULL;
 
 static inline int hashs(char *key) {
    int c = 0, hash = 0;
@@ -24,23 +25,17 @@ bool memo_table_is_set(const char *key) {
 
 void emit_typedefs(Rule **rules, size_t num_rules) {
   int p = 0;
-  while (p++ < num_rules) {
+  while (p++ < num_rules) {     
 	Rule *r = rules[p];
-	printf("typedef struct _%s %s;\n", r->id, r->id);
+
+	if (r->num_types == 1 && r->types[0]->kind = TYPE_PRODUCT)
+	    printf("typedef union _%s %s;\n", r->id, r->id);
+	else
+	    printf("typedef struct _%s %s;\n", r->id, r->id);
 	memo_table_set(r->id);
    }
 }
 
-void emit_cons_enum(Constructor **cons, size_t num_cons) {
-   int p = 0;
-   printf("enum {\n");
-   while (p++ < num_cons) {
-	Consturctor *con = cons[p];
-	if (con->id != NULL)
-		printf("%s_kind,\n", con->id);
-   }
-   printf("\n};\n\n");
-}
 
 char alt_id[MAX_ID] = {0};
 
@@ -54,12 +49,8 @@ static inline char *get_field_name(Field *f, int pos) {
    }
 }
 
-void emit_fields_struct(Field *fields, size_t num_fields) {
-   int p = 0;
-   printf("struct {\n");
-   while (p++ < num_fields) {
-	Field *f = fields[p];
-	switch (f->kind) {
+void emit_single_field(Field *f, int p) {
+  switch (f->kind) {
 	    SEQUENCE:
 		printf("SEQUENCE_FIELD(%s, %s);\n",
 				f->type_id, 
@@ -78,6 +69,32 @@ void emit_fields_struct(Field *fields, size_t num_fields) {
 	        break;
 	    default:
 		break;
-	}
    }
+
 }
+
+void emit_con(Constructor *con, Attributes *attrs) {
+    printf("		struct {\n");
+    for (size_t p = 0; p < con->num_fields; p++)
+	    print_single_field(cons->fields[p], p);
+    printf("		} %s;\n", con->id)
+}
+
+void emit_sum_type(Sum *sum) {
+    printf("struct %s {\n", curr_id);
+    
+
+    printf("	enum {\n");
+    for (size_t i = 0; i < sum->num_cons; i++)
+	    printf("%s_kind, ", sum->cons[i]);
+    printf("	     } kind;\n");
+    
+    printf("	union {\n");
+    for (size_t i = 0; i < sum->num_cons; i++)
+	    emit_con(sum->cons[i], sum->attrs);
+    printf("	\n};");
+
+    printf("\n};\n");
+}
+
+
