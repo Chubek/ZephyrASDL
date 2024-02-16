6294d1f77353a5ba3b3772523c3c70dcd0f50357
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-02-17 01:45:16 +0330

    Fixed: converted buffers to files

--- translate.c
+++ translate.c
@@ -1,18 +1,11 @@
-#define _POSX_C_SOURCE 200112L
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
 
 #include "types.h"
-
-#alloc typedef_heap, typedef_alloc, typedef_realloc, typedef_dump
-
-#hashfunc  translate_hash
-
 
 #ifndef MAX_ID
 #define MAX_ID (1 << 8)
@@ -24,10 +17,6 @@
 
 #ifndef MAX_SEQUENCE_SIZE
 #define MAX_SEQUENCE_SIZE (1 << 10)
-#endif
-
-#ifndef MAX_TYDEF
-#define MAX_TYDEF (1 << 12)
 #endif
 
 const char *macro_normal_field = "#define NORMAL_FIELD(type, name) type name;\n\n";
@@ -68,15 +57,13 @@ static char upper_to_lower_map[SCHAR_MAX] = {
 
 static char *curr_id = NULL;
 
-static char *type_defs = NULL;
-static int type_defs_len = MAX_TYDEF;
-static int type_defs_curs = 0;
-
 
 static int indent = 0;
 static fpos_t top;
 
-static FILE *temp = NULL;
+static FILE *tmp_defs = NULL;
+static FILE *tmp_decls = NULL;
+
 extern FILE *yyout;
 
 static inline char *to_lowercase(char *s, char *buff) {
@@ -94,21 +81,34 @@ static inline char *to_lowercase(char *s, char *buff) {
 
 static inline void print_indent() {
     for (int i = 0; i < indent; i++) {
-        fprintf(temp, "  ");
+        fprintf(tmp_defs, "  ");
     }
 }
 
-#define EMIT(s, ...)           			\
+#define EMIT_DEF(s, ...)           			\
 	do {					\
             print_indent();    	        	\
-	    fprintf(temp, s, __VA_ARGS__);	\
+	    fprintf(tmp_defs, s, __VA_ARGS__);	\
 	} while (0)
 
-#define PUTS(s)					\
+#define PUTS_DEF(s)					\
 	do {					\
 	   print_indent();			\
-	   fputs(s, temp);			\
+	   fputs(s, tmp_defs);			\
 	} while (0)
+
+#define EMIT_DECL(s, ...)           			\
+	do {					\
+            print_indent();    	        	\
+	    fprintf(tmp_decls, s, __VA_ARGS__);	\
+	} while (0)
+
+#define PUTS_DECL(s)					\
+	do {					\
+	   print_indent();			\
+	   fputs(s, tmp_decls);			\
+	} while (0)
+
 
 static inline char *get_field_name(Field *f, int pos, char *buff) {
     if (f->id != NULL)
@@ -125,13 +125,13 @@ void install_field(Field *f, int p) {
     char buff[MAX_ID] = {0};
     switch (f->kind) {
         case SEQUENCE:
-      	    EMIT("SEQUENCE_FIELD(%s, %s);\n", f->type_id, get_field_name(f, p, &buff[0]));
+      	    EMIT_DEF("SEQUENCE_FIELD(%s, %s);\n", f->type_id, get_field_name(f, p, &buff[0]));
             break;
 	case OPTIONAL:
-            EMIT("OPTIONAL_FIELD(%s, %s);\n", f->type_id, get_field_name(f, p, &buff[0]));
+            EMIT_DEF("OPTIONAL_FIELD(%s, %s);\n", f->type_id, get_field_name(f, p, &buff[0]));
             break;
         case NORMAL:
-            EMIT("NORMAL_FIELD(%s, %s);\n", f->type_id, get_field_name(f, p, &buff[0]));
+            EMIT_DEF("NORMAL_FIELD(%s, %s);\n", f->type_id, get_field_name(f, p, &buff[0]));
             break;
         default:
             break;
@@ -142,30 +142,34 @@ void install_field(Field *f, int p) {
 
 void install_consfn(Constructor *con) {
     char buff[MAX_ID];
-    EMIT("%s_tyy create_%s(", curr_id, to_lowercase(con->id, &buff[0]));
+    EMIT_DEF("%s_tyy create_%s(", curr_id, to_lowercase(con->id, &buff[0]));
+    EMIT_DECL("%s_tyy create_%s(", curr_id, to_lowercase(con->id, &buff[0]));
+
 
     for (size_t p = 0; p < con->num_fields; p++) {
-        EMIT("%s %s", con->fields[p]->type_id, get_field_name(con->fields[p], p, &buff[0]));
+        EMIT_DEF("%s %s", con->fields[p]->type_id, get_field_name(con->fields[p], p, &buff[0]));
+        EMIT_DECL("%s %s", con->fields[p]->type_id, get_field_name(con->fields[p], p, &buff[0]));
         if (p < con->num_fields - 1) {
-            fputc(',', temp);
-	    fputc(' ', temp);
+	    PUTS_DEF(", ");
+	    PUTS_DECL(", ");
 	}
     }
 
-    PUTS(") {\n");
+    PUTS_DEF(") {\n");
+    PUTS_DECL(");\n");
 
     indent++;
     
-    EMIT("%s *p = NULL;\n", curr_id);
-    EMIT("p = ALLOC(sizeof(%s));\n", curr_id);
-    EMIT("p->kind = %s_kind;\n", con->id);
+    EMIT_DEF("%s *p = NULL;\n", curr_id);
+    EMIT_DEF("p = ALLOC(sizeof(%s));\n", curr_id);
+    EMIT_DEF("p->kind = %s_kind;\n", con->id);
 
     for (size_t p = 0; p < con->num_fields; p++) {
         char *fldname = get_field_name(con->fields[p], p, &buff[0]);
-        EMIT("p->v.%s.%s = %s;\n", con->id, fldname, fldname);
+        EMIT_DEF("p->v.%s.%s = %s;\n", con->id, fldname, fldname);
     }
 
-    PUTS("return p;\n}\n\n");
+    PUTS_DEF("return p;\n}\n\n");
     
     indent--;
 }
@@ -174,47 +178,47 @@ void install_consfn(Constructor *con) {
 void walk_and_emit_sum_type(Sum *sum) {
     char buff[MAX_ID];
 
-    EMIT("struct _%s {\n", curr_id);
+    EMIT_DEF("struct _%s {\n", curr_id);
 
     indent++;
-    PUTS("enum {\n");
+    PUTS_DEF("enum {\n");
     
     for (size_t i = 0; i < sum->num_cons; i++)
-        EMIT("%s_kind,\n", sum->cons[i]->id);
+        EMIT_DEF("%s_kind,\n", sum->cons[i]->id);
     
     indent--;
-    PUTS("} kind;\n");
+    PUTS_DEF("} kind;\n");
 
-    PUTS("union {\n");
+    PUTS_DEF("union {\n");
 
     indent++;
 
     bool enumerative = false;
     for (size_t i = 0; i < sum->num_cons; i++) {
         if (sum->cons[i]->fields != NULL && !enumerative) {
-            EMIT("struct %s_tyy {\n", sum->cons[i]->id);
+            EMIT_DEF("struct %s_tyy {\n", sum->cons[i]->id);
 	    indent++;
             for (size_t p = 0; p < sum->cons[i]->num_fields; p++)
                 install_field(sum->cons[i]->fields[p], p);
             for (size_t p = 0; p < sum->num_attrs; p++)
                 install_field(sum->attrs[p], p);
-            EMIT("} %s;\n", to_lowercase(&sum->cons[i]->id[0], &buff[0]));
+            EMIT_DEF("} %s;\n", to_lowercase(&sum->cons[i]->id[0], &buff[0]));
 	    indent--;
             continue;
         } else if (sum->cons[i]->fields == NULL && !enumerative) {
             enumerative = true;
-            EMIT("enum %s {\n", sum->cons[i]->id);
+            EMIT_DEF("enum %s {\n", sum->cons[i]->id);
 	    
         }
 
         if (enumerative)
-            EMIT("%s_tyy,\n", sum->cons[i]->id);
+            EMIT_DEF("%s_tyy,\n", sum->cons[i]->id);
     }
-    EMIT("} %s;\n", enumerative ? "" : "v");
+    EMIT_DEF("} %s;\n", enumerative ? "" : "v");
     indent--;
     
 
-    PUTS("};\n\n");
+    PUTS_DEF("};\n\n");
     
 }
 
@@ -226,39 +230,30 @@ void walk_and_emit_sum_consfn(Sum *sum) {
 
 
 void walk_and_emit_prod_type(Product *prod) {
-    EMIT("union %s {\n", curr_id);
+    EMIT_DEF("union %s {\n", curr_id);
 
     indent++;    
     for (size_t p = 0; p < prod->num_fields; p++)
         install_field(prod->fields[p], p);
     indent--;
 
-    PUTS("};\n\n");
+    PUTS_DEF("};\n\n");
 }
 
-void save_typedef(char *id, bool uni) {
-   char buff[MAX_TYDEF] = {0};
-   int def_len = 0;
-
+void emit_typedef(char *id, bool uni) {
    if (uni)
-        def_len = snprintf(&buff[0], MAX_TYDEF, "typedef union _%s %s_tyy;\n", id, id);
+	EMIT_DECL("typedef union _%s %s_tyy;\n", id, id);
    else
-        def_len = snprintf(&buff[0], MAX_TYDEF, "typedef struct _%s %s_tyy;\n", id, id);
-
-
-   type_defs = typedef_realloc(type_defs, type_defs_len + def_len + 1);
-   memmove(&type_defs[type_defs_curs], &buff[0], def_len);
-   type_defs_len += def_len;
-   type_defs_curs += def_len;
+        EMIT_DECL("typedef struct _%s %s_tyy;\n", id, id);
 }
 
 
 
 void translate_rule(char *id, Type *t) {     
     if (t->kind == TYPE_PRODUCT)
-	save_typedef(id, true);
+	emit_typedef(id, true);
     else
-	save_typedef(id, false);
+	emit_typedef(id, false);
 
     curr_id = id;
 
@@ -273,33 +268,30 @@ void translate_rule(char *id, Type *t) {
 
 
 void initialize(void) {
-    temp = tmpfile();
+    tmp_defs = tmpfile();
+    tmp_decls = tmpfile();
 
-    EMIT("#define MAX_SEQUENCE_SIZE %d\n\n", MAX_SEQUENCE_SIZE);
-    PUTS(macro_normal_field);
-    PUTS(macro_sequence_field);
-    PUTS(macro_optional_field);
-    
-    fputs("\n\n", temp);
-
-    fgetpos(temp, &top);
-
-    type_defs = typedef_alloc(type_defs_len);
-	
+    EMIT_DECL("#define MAX_SEQUENCE_SIZE %d\n\n", MAX_SEQUENCE_SIZE);
+    PUTS_DECL(macro_normal_field);
+    PUTS_DECL(macro_sequence_field);
+    PUTS_DECL(macro_optional_field);
 }
 
-void finish_up_translate(void) {
-   rewind(temp);
-   rewind(yyout);
-
-   fputs(type_defs, yyout);
-   fputs("\n\n", yyout);
+void merge_temp_files(void) {
+   rewind(tmp_decls);
+   rewind(tmp_defs);
 
    char c = 0;
-   while ((c = getc(temp)) != EOF)
+
+   while ((c = getc(tmp_decls)) != EOF)
 	fputc(c, yyout);
 
-   fclose(temp);
-   typedef_dump();
+   fputs("\n\n", yyout);
+
+   while ((c = getc(tmp_defs)) != EOF)
+	fputc(c, yyout);
+
+   fclose(tmp_defs);
+   fclose(tmp_decls);
 }
 
