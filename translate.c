b8204f8eec752a42b9810229225c572862a98c7a
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-02-16 04:54:37 +0330

    Fixed: indentation, added upper-to-lower translation

--- translate.c
+++ translate.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 #include "types.h"
 
@@ -14,6 +15,34 @@
 #define MAX_TBL (1 << 14)
 #endif
 
+static char upper_to_lower_map[SCHAR_MAX] = {
+    ['A'] = 'a',
+    ['B'] = 'b',
+    ['C'] = 'c',
+    ['D'] = 'd',
+    ['E'] = 'e',
+    ['F'] = 'f',
+    ['G'] = 'g',
+    ['H'] = 'h',
+    ['I'] = 'i',
+    ['J'] = 'j',
+    ['K'] = 'k',
+    ['L'] = 'l',
+    ['M'] = 'm',
+    ['N'] = 'n',
+    ['O'] = 'o',
+    ['P'] = 'p',
+    ['Q'] = 'q',
+    ['R'] = 'r',
+    ['S'] = 's',
+    ['T'] = 't',
+    ['U'] = 'u',
+    ['V'] = 'v',
+    ['W'] = 'w',
+    ['X'] = 'x',
+    ['Y'] = 'y',
+    ['Z'] = 'z'
+};
 
 char *curr_id = NULL;
 int indent = 0;
@@ -22,17 +51,35 @@ fpos_t curr;
 
 extern FILE *yyout;
 
+static inline char *to_lowercase(char *s, char *buff) {
+   memset(buff, 0, MAX_ID);
+   for (int i = 0; i < strlen(s); i++) {
+	if (s[i] <= 'Z' && s[i] >= 'A') {
+	     buff[i] = upper_to_lower_map[s[i]];
+	} else {
+	     buff[i] = s[i];
+	}
+   }
+
+   return &buff[0];
+}
+
 static inline void print_indent() {
     for (int i = 0; i < indent; i++) {
         fprintf(yyout, "  ");
     }
 }
 
-
 #define EMIT(s, ...)           			\
 	do {					\
             print_indent();    	        	\
 	    fprintf(yyout, s, __VA_ARGS__);	\
+	} while (0)
+
+#define PUTS(s, f)				\
+	do {					\
+	   print_indent();			\
+	   fputs(s, f);				\
 	} while (0)
 
 static inline char *get_field_name(Field *f, int pos, char *buf) {
@@ -46,6 +93,7 @@ static inline char *get_field_name(Field *f, int pos, char *buf) {
 }
 
 void install_field(Field *f, int p) {
+    indent++;
     char buf[MAX_ID] = {0};
     switch (f->kind) {
         case SEQUENCE:
@@ -60,6 +108,7 @@ void install_field(Field *f, int p) {
         default:
             break;
     }
+    indent--;
 }
 
 
@@ -73,8 +122,10 @@ void install_consfn(Constructor *con) {
             putchar(',');
     }
 
-    fputs(") {\n", yyout);
+    PUTS(") {\n", yyout);
+
     indent++;
+    
     EMIT("%s *p = NULL;\n", curr_id);
     EMIT("p = ALLOC(sizeof(%s));\n", curr_id);
     EMIT("p->kind = %s_kind;\n", con->id);
@@ -84,38 +135,46 @@ void install_consfn(Constructor *con) {
         EMIT("p->v.%s.%s = %s;\n", con->id, fldname, fldname);
     }
 
-    fputs("return p;\n}\n", yyout);
-    indent--;
+    PUTS("return p;\n}\n", yyout);
     
+    indent--;
 }
 
 
 void walk_and_emit_sum_type(Sum *sum) {
+    char buff[MAX_ID];
+
     EMIT("struct %s {\n", curr_id);
 
-    fputs("enum {\n", yyout);
     indent++;
+    PUTS("enum {\n", yyout);
+    
     for (size_t i = 0; i < sum->num_cons; i++)
         EMIT("%s_kind,\n", sum->cons[i]->id);
+    
     indent--;
-    fputs("} kind;\n", yyout);
+    PUTS("} kind;\n", yyout);
 
-    fputs("union {\n", yyout);
+    PUTS("union {\n", yyout);
+
     indent++;
+
     bool enumerative = false;
     for (size_t i = 0; i < sum->num_cons; i++) {
         if (sum->cons[i]->fields != NULL && !enumerative) {
             EMIT("struct %s_tyy {\n", sum->cons[i]->id);
+	    indent++;
             for (size_t p = 0; p < sum->cons[i]->num_fields; p++)
                 install_field(sum->cons[i]->fields[p], p);
             for (size_t p = 0; p < sum->num_attrs; p++)
                 install_field(sum->attrs[p], p);
-            EMIT("} %s;\n", sum->cons[i]->id);
+            EMIT("} %s;\n", to_lowercase(&sum->cons[i]->id[0], &buff[0]));
+	    indent--;
             continue;
         } else if (sum->cons[i]->fields == NULL && !enumerative) {
             enumerative = true;
             EMIT("enum %s {\n", sum->cons[i]->id);
-	    indent++;
+	    
         }
 
         if (enumerative)
@@ -123,9 +182,10 @@ void walk_and_emit_sum_type(Sum *sum) {
     }
     EMIT("} %s;\n", enumerative ? "" : "v");
     indent--;
+    
 
-    fputs("};\n", yyout);
-    indent--;
+    PUTS("};\n", yyout);
+    
 }
 
 
@@ -138,12 +198,12 @@ void walk_and_emit_sum_consfn(Sum *sum) {
 void walk_and_emit_prod_type(Product *prod) {
     EMIT("union %s {\n", curr_id);
 
-    indent++;
+    indent++;    
     for (size_t p = 0; p < prod->num_fields; p++)
         install_field(prod->fields[p], p);
     indent--;
 
-    fputs("};\n", yyout);
+    PUTS("};\n", yyout);
 }
 
 
