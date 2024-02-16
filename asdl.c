9be7a180ba21e2622a12a9f9469ab5182c242d0f
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-02-16 06:51:33 +0330

    Added: printout, option for yyout

--- asdl.c
+++ asdl.c
@@ -9,22 +9,35 @@
 #include "types.h"
 
 extern FILE *yyin;
+extern FILE *yyout;
 extern int yyparse(void);
 extern void dump_heaps(void);
+
+extern const char *macro_normal_field;
+extern const char *macro_sequence_field;
+extern const char *macro_optional_field;
 
 int main(int argc, char **argv) {
   int c = 0;
   yyin = stdin;
+  yyout = stdout;
 
   while ((c = getopt(argc, argv, "i:o:")) != -1) {
     switch (c) {
     case 'i':
       yyin = fopen(optarg, "r");
       break;
+    case 'o':
+      yyout = fopen(optarg, "w");
+      break;
     default:
       break;
     }
   }
+
+  fputs(macro_normal_field, yyout);
+  fputs(macro_sequence_field, yyout);
+  fputs(macro_optional_field, yyout);
 
   yyparse();
 
