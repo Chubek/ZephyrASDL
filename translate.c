eced7801754a8458b9851d962f97dc8a8fd4a322
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-02-23 12:40:12 +0330

    Added: additional functions for Translate structure (finalize and dump)

--- translate.c
+++ translate.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "asdl.h"
 
@@ -16,4 +17,40 @@ void init_translator(void) {
   translator.appendage = tmpfile();
   translator.rules = NULL;
   translator.outpath = NULL;
+}
+
+void finalize_translator(void) {
+  FILE *outfile = stdout;
+  int c;
+
+  if (translator.outpath != NULL)
+    if (access(translator.outpath, F_OK | W_OK) == 0)
+      outfile = fopen(translator.outpath, "w");
+
+  while ((c = fgetc(translator.prelude) != EOF)
+		  fputc(c, outfile);
+
+  fputc('\n', outfile);
+  
+  while ((c = fgetc(translator.decls) != EOF)
+		  fputc(c, outfile);
+
+  fputc('\n', outfile);
+
+  while ((c = fgetc(translator.defs) != EOF)
+		  fputc(c, outfile);
+
+  fputc('\n', outfile);
+  
+  while ((c = fgetc(translator.appendage) != EOF)
+		  fputc(c, outfile);
+
+  fputc(c, outfile);
+}
+
+void dump_translator(void) {
+  fclose(translator.prelude);
+  fclose(translator.decls);
+  fclose(translator.defs);
+  fclose(translator.appendage);
 }
