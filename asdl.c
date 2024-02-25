5367b8f7ee816d21bea6892ded49285d357a69c9
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-02-25 06:59:39 +0330

    Added: print help function

--- asdl.c
+++ asdl.c
@@ -5,11 +5,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sysexits.h>
 
 #include "asdl.h"
 
 extern FILE *yyin;
 extern int yyparse(void);
+
+
+static inline void print_help_and_exit() {
+    printf("Usage: asdl [-o output] [-d def_suffix] [-f fn_suffix] [-k kind_suffix] [-a arg_suffix] FILE\n");
+    printf("\n");
+    printf("Options:\n");
+    printf("  -o output      Specify the output file\n");
+    printf("  -d def_suffix  Specify the suffix for typedefs\n");
+    printf("  -f fn_suffix   Specify the suffix for functions\n");
+    printf("  -k kind_suffix Specify the suffix for kind types\n");
+    printf("  -a arg_suffix  Specify the suffix for arguments\n");
+    printf("\n");
+    exit(EX_USAGE);
+}
 
 void parse_arguments(int argc, char **argv) {
   int c = 0;
